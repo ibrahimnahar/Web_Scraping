a79b7fe8c3281f078d3c8feed4c68a09ca4d81d0
ibrahimnahar <ibrahim.ali.nahar@gmail.com>
2023-12-28 12:37:45 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Function to scrape data from a book's individual URL
+def scrape_book_details(book_url):
+    response = session.get(book_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'lxml')
+        table_raws = soup.css.select('table tr')
+        # Extract additional data as needed
+        return {'title': soup.css.select_one('.product_main h1').text.strip(),
+               'product_type': table_raws[1].css.select_one('td').text.strip(),
+               'price_excl_tax' : table_raws[2].css.select_one('td').text.strip(),
+               'price_incl_tax' : table_raws[3].css.select_one('td').text.strip(),
+               'tax' : table_raws[4].css.select_one('td').text.strip(),
+               'availability' : table_raws[5].css.select_one('td').text.strip(),
+               'num_reviws' : table_raws[6].css.select_one('td').text.strip(),
+               'description': soup.css.select_one('.sub-header').next_sibling.next_sibling.text.strip(),
+               'stars': soup.css.select_one('.product_main p.star-rating')['class'][1],
+               'category': soup.css.select_one('li.active').previous_sibling.previous_sibling.a.text.strip(),
+               }
+    else:
+        print(f"Error fetching details for {book_url}: {response.status_code}")
+        return None
+
+# Make a request to the URL
+def scrape_books(url):
+    response = session.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        # Work with the BeautifulSoup object
+        books = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
+        for book in books:
+            relative_url = book.h3.a['href']
+            # Scrape additional data from each book's URL
+            if 'catalogue' in relative_url:
+                book_url = 'https://books.toscrape.com/'+relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/'+relative_url
+
+            yield scrape_book_details(book_url)
+
+        next_page = soup.find('li', class_='next')
+        print('*'*40)
+        if next_page is not None:
+            next_page = next_page.a['href']
+            if 'catalogue' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield from scrape_books(next_page_url)
+    else:
+        # Print an error message if the request was not successful
+        print(f"Error: {response.status_code}")
+
+# Starting URL
+def main():
+    url = "https://books.toscrape.com/"
+    books_info = scrape_books(url)
+    info = [book for book in books_info]
+    df = pd.DataFrame(info)
+    df.to_csv('books.csv', index=False)
+    print(f'Data written to books.csv')
+# Use a session for requests
+session = requests.Session()
+main()
